9e3ff8b7b2b1cedda44ae2c22158708f4d37b1ad
Roger David Alba Ortega <rogerdavidalbaortega@gmail.com>
2023-10-14 17:40:22 -0500

    Create component navigation with its styles and responsive design

---
+++ src/components/MenuNavigation.vue
@@ -0,0 +1,42 @@
+<script setup>
+import IconMessage from './icons/IconMessage.vue'
+import IconNotification from './icons/IconNotification.vue'
+import IconCompany from './icons/IconCompany.vue'
+import IconMenu from './icons/IconMenu.vue'
+import IconArrowDown from './icons/IconArrowDown.vue'
+import IconPlus from './icons/IconPlus.vue'
+
+</script>
+
+<template>
+  <nav class="bg-blue-primary px-4 h-14 flex flex-row justify-between items-center">
+    <IconMenu class="ml-4 lg:hidden" />
+    <a href="https://website.talentpitch.co/" class="hidden md:block pl-4">
+      <IconCompany />
+    </a>
+    <div class="lg:hidden"></div>
+    <menu class="hidden lg:block">
+      <ul class="flex flex-row justify-between gap-x-4 text-text-nav">
+        <li class="hover:text-button-primary">
+          <router-link to="/">Conéctate</router-link>
+        </li>
+        <li class="hover:text-button-primary">
+          <router-link to="/">Mis Muros</router-link>
+        </li>
+        <li class="hover:text-button-primary">
+          <router-link to="/">Mis Listas</router-link>
+        </li>
+        <li class="hover:text-button-primary">
+          <router-link to="/">Aprende</router-link>
+        </li>
+      </ul>
+    </menu>
+    <div class="hidden lg:flex flex-row justify-between items-center gap-x-4 text-text-nav">
+      <button class="bg-button-primary flex justify-center items-center gap-x-2 px-3 py-1 rounded">Comenzar <IconPlus/></button>
+      <IconMessage class="text-black" />
+      <IconNotification />
+      <img src="../assets/profile.png" alt="User Profile" />
+      <IconArrowDown class="mr-4"/>
+    </div>
+  </nav>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    stroke: (theme) => ({
+      white: theme('colors.white')
+    }),
+    extend: {},
+    colors: {
+      blue: {
+        primary: '#1a237e'
+      },
+      text: {
+        nav: '#9fa8da'
+      },
+      button: {
+        primary: '#de1c7d'
+      }
+    }
   },
   plugins: []
 }
